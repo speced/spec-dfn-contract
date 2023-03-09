1cedbe1004ea4c5401e435ec187468df84b34af3
Marcos Cáceres <marcos@marcosc.com>
2023-03-09 09:10:28 -0800

    Add the initial dfn specification and a test framework (#10)
    Co-authored-by: François Daoust <fd@tidoust.net>
    Co-authored-by: Tab Atkins Jr. <jackalmage@gmail.com>

---
+++ spec/definition-export-spec.js
@@ -0,0 +1,33 @@
+const sources = ["bikeshed", "respec"];
+
+const docTypes = new Map();
+beforeAll(async () => {
+  for (let source of sources) {
+    docTypes.set(source, createIframe(source));
+  }
+});
+
+afterAll(async () => {
+  const iFrames = await Promise.all([...docTypes.values()]);
+  cleanUp(iFrames);
+});
+
+for (const type of sources) {
+  describe(`${type} - simple definition`, () => {
+    it(`must have data-dfn-type attribute`, async () => {
+      const iframe = await docTypes.get(type);
+      const doc = iframe.contentDocument;
+      const dfn = doc.querySelector(
+        "#simple-definition dfn[data-dfn-type=dfn]"
+      );
+      expect(dfn).withContext(type).not.toBeNull();
+    });
+
+    it(`must have an id attribute`, async () => {
+      const iframe = await docTypes.get(type);
+      const doc = iframe.contentDocument;
+      const dfn = doc.querySelector("#simple-definition dfn[id]");
+      expect(dfn).not.toBeNull();
+    });
+  });
+}
---
+++ spec/helpers/test-utils.js
@@ -0,0 +1,13 @@
+async function createIframe(type) {
+  const iframe = document.createElement("iframe");
+  iframe.src = `out/${type}.html`;
+  document.body.appendChild(iframe);
+  await new Promise((r) => (iframe.onload = r));
+  return iframe;
+}
+
+async function cleanUp(iFrames) {
+  for (const iframe of iFrames) {
+    iframe.remove();
+  }
+}
